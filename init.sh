a2ac33cf00f787a77eb12dc68856ed678afba59d
ThisIs-MyName <user@vanilla.com>
2015-02-23 16:50:21 -0500

    It builds\!

--- init.sh
+++ init.sh
@@ -1,3 +1,38 @@
+set -o verbose
 wget ftp://invisible-island.net/ncurses/ncurses.tar.gz &&
 tar xf ncurses.tar.gz &&
 rm ncurses.tar.gz
+
+#cd curl
+#./buildconf
+#./configure --prefix=$PWD/../out --quiet &&
+#make clean --silent &&
+#make --silent &&
+#make test --silent &&
+#make install --silent
+#cd ..
+
+cd ncurses-5.9
+./configure --prefix=$PWD/../out --quiet &&
+make clean --silent &&
+make --silent &&
+make install --silent
+cd ..
+
+cd libtorrent
+./autogen.sh &&
+./configure --prefix=$PWD/../out --quiet &&
+make clean --silent &&
+make --silent &&
+make install --silent
+cd ..
+
+cd rtorrent
+./autogen.sh &&
+./configure CPPFLAGS="-I$PWD/../out/include -I$PWD/../out/include/ncurses" LDFLAGS="-L$PWD/../out/lib" libtorrent_CFLAGS="-I$PWD/../out/include" libtorrent_LIBS="-L$PWD/../out/lib -ltorrent" --prefix=$PWD/../out &&
+make clean --silent &&
+make --silent &&
+make install --silent
+cd ..
+
+echo "==DONE=="
